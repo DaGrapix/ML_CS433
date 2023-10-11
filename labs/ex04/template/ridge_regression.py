241b7db8b9ee0e8a6ab75a5d3a7a732f0c0b7c2d
starkiller <anthony.kalaydjian@outlook.com>
2023-10-11 23:06:38 +0200

    ex7

---
+++ labs/ex04/template/ridge_regression.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""Exercise 3.
+
+Ridge Regression
+"""
+
+import numpy as np
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+    N = len(y)
+    w_opt   = np.linalg.solve((tx.T@tx + 2*N*lambda_*np.eye(tx.shape[1])), tx.T@y)
+    rmse     = np.sqrt(np.mean((y - tx@w_opt)**2))
+    
+    return w_opt
